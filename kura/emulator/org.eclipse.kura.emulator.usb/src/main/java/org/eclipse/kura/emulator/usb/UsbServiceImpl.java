eb1f1f780a1de9b8d37c293a19f0a032ffd4af39
Jens Reimann <jreimann@redhat.com>
2016-07-15 15:33:01 +0200

    Clean up warnings
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- kura/emulator/org.eclipse.kura.emulator.usb/src/main/java/org/eclipse/kura/emulator/usb/UsbServiceImpl.java
+++ kura/emulator/org.eclipse.kura.emulator.usb/src/main/java/org/eclipse/kura/emulator/usb/UsbServiceImpl.java
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *     Eurotech
+ *     Jens Reimann <jreimann@redhat.com> - Clear warnings
  *******************************************************************************/
 package org.eclipse.kura.emulator.usb;
 
@@ -22,52 +23,38 @@ import org.eclipse.kura.usb.UsbNetDevice;
 import org.eclipse.kura.usb.UsbService;
 import org.eclipse.kura.usb.UsbTtyDevice;
 import org.osgi.service.component.ComponentContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UsbServiceImpl implements UsbService {
 	
-	private static final Logger s_logger = LoggerFactory.getLogger(UsbServiceImpl.class);
-
-	private List m_usbDevices;		//udev doesn't properly track devices (particularly for removal events) - so we have to do it.
-	
 	protected void activate(ComponentContext componentContext) {
-		//only support Linux
-		//Properties props = System.getProperties();
 	}
 
 	protected void deactivate(ComponentContext componentContext) {
-		m_usbDevices = null;
 	}
 
 	@Override
 	public UsbServices getUsbServices()
 			throws KuraException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<? extends UsbDevice> getUsbDevices() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<UsbBlockDevice> getUsbBlockDevices() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<UsbNetDevice> getUsbNetDevices() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<UsbTtyDevice> getUsbTtyDevices() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
