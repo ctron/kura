35082ef1ac61d11976384ad744e97a05890669c1
Jens Reimann <jreimann@redhat.com>
2016-12-16 14:28:54 +0100

    Drop ant generated DP bundles in favor of Maven build
    This change replaces the custom DPs assembly based on a few ant scripts
    with using a Maven plugin.
    
    This also allows the DPs to be built during a -SNAPSHOT build.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Copyright (c) 2016 Red Hat and/or its affiliates
+# Copyright (c) 2016 Red Hat Inc
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
